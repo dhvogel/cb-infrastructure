0e5060d3a6cf086e75f59e142cb101007419967b
dhvogel <dhvogel2468@gmail.com>
2018-04-30 21:56:08 -0700

    modify userdata

--- cb-karl-instance/userdata.sh
+++ cb-karl-instance/userdata.sh
@@ -6,3 +6,7 @@ sudo apt-get -y update
 sudo wget -qO- https://get.docker.com/ | sh
 # add user to docker group
 sudo usermod -aG docker $(whoami)
+# docker login
+docker login -u cbikesbot -p cbikes94
+# docker run latest cb-karl
+docker run -e NODE_ENV=test -p 80:3000 dhvogel/cb-karl:latest
